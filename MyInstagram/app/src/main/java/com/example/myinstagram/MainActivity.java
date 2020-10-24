803b17cdd243370b87e7d28a0288dc764dcbd9f1
ZhouXing19 <zhouxing@uchicago.edu>
2020-10-24 00:29:32 -0500

    Get posts from backend

--- MyInstagram/app/src/main/java/com/example/myinstagram/MainActivity.java
+++ MyInstagram/app/src/main/java/com/example/myinstagram/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.myinstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage = findViewById(R.id.btnCaptureImage);
         ivPostImage = findViewById(R.id.ivPic);
         btnSubmit = findViewById(R.id.btnSubmit);
+        
+        queryPosts();
+    }
+
+    private void queryPosts() {
+
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e!=null){
+                    Log.e(TAG, "Issues with getting posts", e );
+                    return;
+                }
+                for(Post post:posts){
+                    Log.i(TAG, "done: Post: " + post.getDescription());
+                }
+
+            }
+        });
     }
 }
